4ed985c3fa44b6d0d7183fd5524f787a5ab9cd9d
Toxic-Whitlgnd <whitelegend56@gmail.com>
2024-08-11 00:07:17 +0530

    Hover card V2 has been added and code has been moved to one level up

---
+++ src/models/HoverCardV1props.ts
@@ -0,0 +1,26 @@
+
+export interface HovCardProps {
+    cardWidth?: string;
+    cardHeight?: string;
+    cardPadding?: string;
+    cardBorderWidth?: string;
+    cardBorderColor?: string;
+    cardBorderStyle?: string;
+    cardBorderRadius?: string;
+    cardEndScale?: string;
+    cardStartScale?: string;
+    cardImgContent?: string;
+    cardImgWidth?: string;
+    cardImgHeight?: string;
+    cardTitle?: string;
+    cardTitleFontSize?: string;
+    cardDescpriptionFontFamily?: string;
+    cardDescpription?: string;
+    cardRandomText?: string;
+    link?: string;
+    cardBtnName?: string;
+    cardBtnColor?: string;
+    cardBtnBackgroundColor?: string;
+    cardBtnSize?: string;
+    cardBtnBorderRadius?: string;
+}
